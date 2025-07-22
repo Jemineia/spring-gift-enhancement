7c6375868b64684d82f3c379e651e5539d21a824
DESKTOP-DHJI2MJ\user <choroc2020@gmail.com>
2025-07-22 17:30:44 +0900

    [Step3:feat] TestCode 수행전 Context 초기화

--- src/test/java/gift/ProductControllerTest.java
+++ src/test/java/gift/ProductControllerTest.java
@@ -10,12 +10,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
 @DisplayName("Product 삽입 Test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)  // context 오염 방지
 public class ProductControllerTest {
 
   @Autowired
@@ -23,14 +25,13 @@ public class ProductControllerTest {
 
   @Test
   @DisplayName("정상 물건 생성")
-  void validProductName() throws Exception{
+  void validProductName() throws Exception {
     // given
     String name = "초코파이";
     int price = 5700;
     String imageUrl = "https://example.com/image.jpg";
     String option = "추석 선물용 500KG";
     int optionQuantity = 72;
-
 
     mockMvc.perform(post("/admin/products")
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
@@ -45,7 +46,7 @@ public class ProductControllerTest {
 
   @Test
   @DisplayName("[Product.name] 최대 글자 수 길이 제한 초과")
-  void overLength_ProductName() throws Exception{
+  void overLength_ProductName() throws Exception {
     // given
     String name = "초코파이는15자가넘을까요안넘을까요";
 
@@ -67,7 +68,7 @@ public class ProductControllerTest {
 
   @Test
   @DisplayName("[Product.name] 허용되지 않은 특수문자 사용")
-  void invalidHyperText_ProductName() throws  Exception{
+  void invalidHyperText_ProductName() throws Exception {
     // given
     String name = "초코파이!@#$%**";
 
@@ -89,7 +90,7 @@ public class ProductControllerTest {
 
   @Test
   @DisplayName("[Product.name] '카카오'가 상품명에 포함")
-  void invalidWord_ProductName() throws  Exception {
+  void invalidWord_ProductName() throws Exception {
     // given
     String name = "카카오 초코파이";
 
@@ -111,7 +112,7 @@ public class ProductControllerTest {
 
   @Test
   @DisplayName("[Product.price] 음수 가격 삽입")
-  void minusPrice_ProductPrice() throws  Exception{
+  void minusPrice_ProductPrice() throws Exception {
     // given
     int price = -7200;
 
@@ -133,7 +134,7 @@ public class ProductControllerTest {
 
   @Test
   @DisplayName("[Product.option] 옵션을 비우고 상품 등록")
-  void inValidOptionInsert() throws  Exception{
+  void inValidOptionInsert() throws Exception {
     // given
     int price = 7200;
     String name = "초코파이";
